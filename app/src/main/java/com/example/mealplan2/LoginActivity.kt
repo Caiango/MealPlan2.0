3fe35f0b33ae0005cfab0f721ae1a083f1564ef1
Caiango <cvff12345@gmail.com>
2020-10-07 21:40:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mealplan2/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mealplan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
