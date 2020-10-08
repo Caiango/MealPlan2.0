42a4627b6c089c910bb4172a190aa1ba13fae5d9
Caiango <cvff12345@gmail.com>
2020-10-08 01:34:16 -0300

    Criando tela LoggedActivity.kt

--- app/src/main/java/com/example/mealplan2/LoginActivity.kt
+++ app/src/main/java/com/example/mealplan2/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mealplan2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
@@ -8,6 +9,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoggedActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
