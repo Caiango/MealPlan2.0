3c5d6fc7f78aaff6d153eb416eed20af14f8f86f
Caiango <cvff12345@gmail.com>
2020-10-08 12:51:21 -0300

    Implementando layout LoggedActivity.kt

--- app/src/main/java/com/example/mealplan2/LoggedActivity.kt
+++ app/src/main/java/com/example/mealplan2/LoggedActivity.kt
@@ -2,10 +2,28 @@ package com.example.mealplan2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_logged.*
 
 class LoggedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged)
+
+        viewPratos.setOnClickListener {
+            Toast.makeText(this, "Gerenciando Pratos", Toast.LENGTH_SHORT).show()
+        }
+
+        viewPedidos.setOnClickListener {
+            Toast.makeText(this, "Gerenciando Pedidos", Toast.LENGTH_SHORT).show()
+        }
+
+        viewHist.setOnClickListener {
+            Toast.makeText(this, "Gerenciando Histórico", Toast.LENGTH_SHORT).show()
+        }
+
+        viewNewLogin.setOnClickListener {
+            Toast.makeText(this, "Gerenciando os Usuários", Toast.LENGTH_SHORT).show()
+        }
     }
 }
