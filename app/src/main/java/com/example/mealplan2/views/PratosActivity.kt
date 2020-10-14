b3b3967769b49175f0def66a031f96c9a1d15182
Caiango <cvff12345@gmail.com>
2020-10-14 13:38:10 -0300

    CRUD completo dos pratos

--- app/src/main/java/com/example/mealplan2/views/PratosActivity.kt
+++ app/src/main/java/com/example/mealplan2/views/PratosActivity.kt
@@ -5,13 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.mealplan2.R
@@ -21,11 +19,15 @@ import kotlinx.android.synthetic.main.activity_history.*
 import kotlinx.android.synthetic.main.activity_pratos.*
 import kotlinx.android.synthetic.main.pratos_dialog.*
 import org.json.JSONArray
+import org.json.JSONObject
 
 class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
     var getdata = mutableListOf<HashMap<String, String>>()
     var url = "http://192.168.1.2/meal_plan2/show_pratos.php"
     var url2 = "http://192.168.1.2/meal_plan2/get_categories.php"
+    var url3 = "http://192.168.1.2/meal_plan2/insert_prato.php"
+    var url4 = "http://192.168.1.2/meal_plan2/delete_prato.php"
+    var url5 = "http://192.168.1.2/meal_plan2/update_prato.php"
 
     var spinnerList: ArrayList<String> = ArrayList()
 
@@ -61,6 +63,10 @@ class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
             dialog.setTitle("Adicionar Prato")
             dialog.setMessage("Preencha os Dados")
             val view = LayoutInflater.from(this).inflate(R.layout.pratos_dialog, null)
+            dialog.setView(view)
+            val prato = view.findViewById<EditText>(R.id.edt_nome_prato)
+            val desc = view.findViewById<EditText>(R.id.edt_desc_prato)
+            val valor = view.findViewById<EditText>(R.id.edt_valor_prato)
 
             //setando spinner
             var spinner = view.findViewById<Spinner>(R.id.spinnerFiltro)
@@ -70,9 +76,14 @@ class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
             adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapterSpinner
 
-            dialog.setView(view)
+
             dialog.setPositiveButton("Adicionar") { _: DialogInterface, _: Int ->
-                Toast.makeText(applicationContext, "Prato Adicionado", Toast.LENGTH_LONG).show()
+//                Toast.makeText(applicationContext, "Prato Adicionado", Toast.LENGTH_LONG).show()
+                insertFood(
+                    prato.text.toString().trim(),
+                    desc.text.toString().trim(),
+                    valor.text.toString().trim()
+                )
             }
             dialog.setNegativeButton("Cancelar") { _: DialogInterface, i: Int ->
                 Toast.makeText(this, "Cancelar", Toast.LENGTH_SHORT).show()
@@ -85,6 +96,7 @@ class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
         val request = StringRequest(
             Request.Method.POST, url,
             { response ->
+                getdata.clear()
                 val jsonArray = JSONArray(response)
                 for (x in 0..(jsonArray.length() - 1)) {
                     val jsonObject = jsonArray.getJSONObject(x)
@@ -92,6 +104,7 @@ class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
                     mhs.put("food_name", jsonObject.getString("food_name"))
                     mhs.put("food_description", jsonObject.getString("food_description"))
                     mhs.put("food_price", jsonObject.getString("food_price"))
+                    mhs.put("foods_id", jsonObject.getString("foods_id"))
                     getdata.add(mhs)
                 }
                 mhsAdapter.notifyDataSetChanged()
@@ -112,6 +125,7 @@ class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
         val prato = view.findViewById<EditText>(R.id.edt_nome_prato)
         val desc = view.findViewById<EditText>(R.id.edt_desc_prato)
         val valor = view.findViewById<EditText>(R.id.edt_valor_prato)
+        var id = item["foods_id"]
 
         //setando spinner
         var spinner = view.findViewById<Spinner>(R.id.spinnerFiltro)
@@ -125,10 +139,20 @@ class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
         desc.setText(item["food_description"])
         valor.setText(item["food_price"])
         dialog.setPositiveButton("Alterar") { _: DialogInterface, _: Int ->
-            Toast.makeText(applicationContext, "Prato Alterado", Toast.LENGTH_LONG).show()
+            if (id != null) {
+                updateFood(
+                    id, prato.text.toString().trim(),
+                    desc.text.toString().trim(),
+                    valor.text.toString().trim()
+                )
+            }
+
         }
         dialog.setNegativeButton("Excluir") { _: DialogInterface, i: Int ->
-            Toast.makeText(this, "Prato Excluído", Toast.LENGTH_SHORT).show()
+            if (id != null) {
+                deleteFood(id)
+            }
+
         }
         dialog.setNeutralButton("Cancelar") { _: DialogInterface, i: Int ->
             Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
@@ -162,6 +186,92 @@ class PratosActivity : AppCompatActivity(), PratosAdapter.onLongClickListener {
             { error ->
                 Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
             })
+        val queue = Volley.newRequestQueue(this)
+        queue.add(request)
+    }
+
+    fun insertFood(nome: String, desc: String, valor: String) {
+        val request = object : StringRequest(Method.POST, url3,
+            Response.Listener { response ->
+                val jsonObject = JSONObject(response)
+                val error = jsonObject.getString("kode")
+                if (error.equals("000")) {
+                    Toast.makeText(applicationContext, "Prato Inserido", Toast.LENGTH_LONG).show()
+                    showDataMhs()
+                    mhsAdapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(applicationContext, "Algo deu errado", Toast.LENGTH_LONG).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_LONG).show()
+            }) {
+            override fun getParams(): MutableMap<String, String> {
+                val hm = HashMap<String, String>()
+                //recebendo e enviando valores para o php
+                hm.put("food_name", nome)
+                hm.put("food_description", desc)
+                hm.put("food_price", valor)
+                return hm
+            }
+        }
+        val queue = Volley.newRequestQueue(this)
+        queue.add(request)
+    }
+
+    fun deleteFood(id: String) {
+        val request = object : StringRequest(Method.POST, url4,
+            Response.Listener { response ->
+                val jsonObject = JSONObject(response)
+                val error = jsonObject.getString("kode")
+                if (error.equals("000")) {
+                    Toast.makeText(applicationContext, "Prato Excluído", Toast.LENGTH_LONG).show()
+                    showDataMhs()
+                    mhsAdapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(applicationContext, "Algo deu errado", Toast.LENGTH_LONG).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_LONG).show()
+            }) {
+            override fun getParams(): MutableMap<String, String> {
+                val hm = HashMap<String, String>()
+                //recebendo e enviando valores para o php
+                hm["foods_id"] = id
+                return hm
+            }
+        }
+        val queue = Volley.newRequestQueue(this)
+        queue.add(request)
+    }
+
+    fun updateFood(id: String, nome: String, desc: String, valor: String) {
+        val request = object : StringRequest(Method.POST, url5,
+            Response.Listener { response ->
+                val jsonObject = JSONObject(response)
+                val error = jsonObject.getString("kode")
+                if (error.equals("000")) {
+                    Toast.makeText(applicationContext, "Prato Alterado", Toast.LENGTH_LONG).show()
+                    showDataMhs()
+                    mhsAdapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(applicationContext, "Algo deu errado", Toast.LENGTH_LONG).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_LONG).show()
+            }) {
+            override fun getParams(): MutableMap<String, String> {
+                val hm = HashMap<String, String>()
+                //recebendo e enviando valores para o php
+                hm["foods_id"] = id
+                hm.put("food_name", nome)
+                hm.put("food_description", desc)
+                hm.put("food_price", valor)
+                return hm
+            }
+        }
         val queue = Volley.newRequestQueue(this)
         queue.add(request)
     }
