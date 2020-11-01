11d98034f20b0a909308d47065a044b83688a422
Caiango <cvff12345@gmail.com>
2020-11-01 17:48:15 -0300

    Criando HistoryController.kt

---
+++ app/src/main/java/com/example/mealplan2/controller/HistoryController.kt
@@ -0,0 +1,78 @@
+package com.example.mealplan2.controller
+
+import android.content.Context
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.mealplan2.adapters.HistAdapter
+import org.json.JSONArray
+import org.json.JSONObject
+import java.lang.Exception
+
+class HistoryController {
+
+    companion object {
+        var getdata = mutableListOf<HashMap<String, String>>()
+    }
+
+//    var url = "http://192.168.0.22/php_android/show_history.php"
+//    var url2 = "http://192.168.0.22/php_android/delete_all_hist.php"
+
+    var url = "http://192.168.1.2/meal_plan2/show_history.php"
+    var url2 = "http://192.168.1.2/meal_plan2/delete_all_hist.php"
+
+    fun showDataMhs(context: Context, adapter: HistAdapter) {
+        val request = StringRequest(
+            Request.Method.POST, url,
+            { response ->
+                getdata.clear()
+                try {
+
+                    val jsonArray = JSONArray(response)
+                    for (x in 0..(jsonArray.length() - 1)) {
+                        val jsonObject = jsonArray.getJSONObject(x)
+                        var mhs = HashMap<String, String>()
+                        mhs.put("table_number", jsonObject.getString("table_number"))
+                        mhs.put("order_description", jsonObject.getString("order_description"))
+                        mhs.put("order_price", jsonObject.getString("order_price"))
+                        mhs.put("order_date", jsonObject.getString("order_date"))
+                        mhs.put("order_time", jsonObject.getString("order_time"))
+                        getdata.add(mhs)
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(context, "Não há histórico", Toast.LENGTH_LONG).show()
+                }
+                adapter.notifyDataSetChanged()
+            },
+            { error ->
+                Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show()
+            })
+        val queue = Volley.newRequestQueue(context)
+        queue.add(request)
+    }
+
+    fun deleteHist(context: Context, adapter: HistAdapter) {
+        val request = object : StringRequest(Method.POST, url2,
+            Response.Listener { response ->
+                val jsonObject = JSONObject(response)
+                val error = jsonObject.getString("kode")
+                if (error.equals("000")) {
+                    Toast.makeText(context, "Histórico Excluído", Toast.LENGTH_LONG)
+                        .show()
+                    showDataMhs(context, adapter)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(context, "Algo deu errado", Toast.LENGTH_LONG).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(context, error.toString(), Toast.LENGTH_LONG).show()
+            }) {
+        }
+        val queue = Volley.newRequestQueue(context)
+        queue.add(request)
+    }
+
+}
--- app/src/main/java/com/example/mealplan2/views/HistoryActivity.kt
+++ app/src/main/java/com/example/mealplan2/views/HistoryActivity.kt
@@ -17,6 +17,7 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.mealplan2.R
 import com.example.mealplan2.adapters.HistAdapter
+import com.example.mealplan2.controller.HistoryController
 import com.example.mealplan2.controller.LoginController
 import kotlinx.android.synthetic.main.activity_history.*
 import org.json.JSONArray
@@ -25,25 +26,21 @@ import java.lang.Exception
 
 class HistoryActivity : AppCompatActivity() {
 
-    var getdata = mutableListOf<HashMap<String, String>>()
-//    var url = "http://192.168.0.22/php_android/show_history.php"
-//    var url2 = "http://192.168.0.22/php_android/delete_all_hist.php"
-
-    var url = "http://192.168.1.2/meal_plan2/show_history.php"
-    var url2 = "http://192.168.1.2/meal_plan2/delete_all_hist.php"
-
     lateinit var mhsAdapter: HistAdapter
+    lateinit var mHistoryController: HistoryController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        mHistoryController = HistoryController()
 
         floatDelHist.setOnClickListener {
             if (LoginController.currentPrivilege == "Administração") {
                 val dialog = AlertDialog.Builder(this)
                 dialog.setTitle("Deseja Limpar o Histórico?")
                 dialog.setPositiveButton("Sim") { _: DialogInterface, _: Int ->
-                    deleteHist()
+                    mHistoryController.deleteHist(this, mhsAdapter)
                 }
                 dialog.setNegativeButton("Cancelar") { _: DialogInterface, i: Int ->
 
@@ -62,71 +59,15 @@ class HistoryActivity : AppCompatActivity() {
 
         rv_hist.layoutManager = LinearLayoutManager(this)
         rv_hist.setHasFixedSize(true)
-        mhsAdapter = HistAdapter(getdata)
+        mhsAdapter = HistAdapter(HistoryController.getdata)
         rv_hist.adapter = mhsAdapter
-        showDataMhs()
+
+        mHistoryController.showDataMhs(this, mhsAdapter)
 
         imageBackHist.setOnClickListener {
             val intent = Intent(this, LoggedActivity::class.java)
             startActivity(intent)
         }
 
-    }
-
-    fun showDataMhs() {
-        val request = StringRequest(
-            Request.Method.POST, url,
-            { response ->
-                getdata.clear()
-                try {
-
-                    val jsonArray = JSONArray(response)
-                    for (x in 0..(jsonArray.length() - 1)) {
-                        val jsonObject = jsonArray.getJSONObject(x)
-                        var mhs = HashMap<String, String>()
-                        mhs.put("table_number", jsonObject.getString("table_number"))
-                        mhs.put("order_description", jsonObject.getString("order_description"))
-                        mhs.put("order_price", jsonObject.getString("order_price"))
-                        mhs.put("order_date", jsonObject.getString("order_date"))
-                        mhs.put("order_time", jsonObject.getString("order_time"))
-                        getdata.add(mhs)
-                    }
-                } catch (e: Exception) {
-                    Toast.makeText(applicationContext, "Não há histórico", Toast.LENGTH_LONG).show()
-                }
-                mhsAdapter.notifyDataSetChanged()
-            },
-            { error ->
-                Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
-            })
-        val queue = Volley.newRequestQueue(this)
-        queue.add(request)
-    }
-
-    fun deleteHist() {
-        val request = object : StringRequest(Method.POST, url2,
-            Response.Listener { response ->
-                val jsonObject = JSONObject(response)
-                val error = jsonObject.getString("kode")
-                if (error.equals("000")) {
-                    Toast.makeText(applicationContext, "Histórico Excluído", Toast.LENGTH_LONG)
-                        .show()
-                    showDataMhs()
-                    mhsAdapter.notifyDataSetChanged()
-                } else {
-                    Toast.makeText(applicationContext, "Algo deu errado", Toast.LENGTH_LONG).show()
-                }
-            },
-            Response.ErrorListener { error ->
-                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_LONG).show()
-            }) {
-//            override fun getParams(): MutableMap<String, String> {
-//                val hm = HashMap<String, String>()
-//
-//                return hm
-//            }
-        }
-        val queue = Volley.newRequestQueue(this)
-        queue.add(request)
     }
 }
